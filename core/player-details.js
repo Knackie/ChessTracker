dc54b7c5e5c1814eec584c5de7df376120d05865
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-10-08 16:50:05 +0200

    Change the content of the page on player details clicked

--- core/player-details.js
+++ core/player-details.js
@@ -3,4 +3,34 @@ const navigateToPlayerDetails = (el, getNameFrom) => {
 
   history.pushState({}, "", `#${playerName}`);
   document.title = `♟️ Chess Tracker - ${playerName}`;
+
+  showPlayerDetails(playerName);
+};
+
+const showPlayerDetails = (playerName) => {
+  const detailsTemplate = `
+    <div class="details">
+      <h1>${playerName}</h1>
+      <a href="classement">Voir le classement complet</a>
+    </div>
+    `;
+
+  replaceBodyWith(detailsTemplate);
+};
+
+const replaceBodyWith = (template) => {
+  const navbarTemplate = `
+    <div class="w3-bar w3-green">
+      <a href="/ChessTracker" class="w3-bar-item w3-button">Accueil</a>
+      <a href="classement" class="w3-bar-item w3-button">Classement</a>
+      <a href="historique" class="w3-bar-item w3-button">Historique</a>
+      <a
+        href="https://github.com/Knackie/ChessTracker"
+        class="w3-bar-item w3-button"
+        >Github</a
+      >
+    </div>
+    `;
+
+  document.body.innerHTML = navbarTemplate + template;
 };

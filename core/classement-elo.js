e2af7011ec1fbe29e7864b58407357ac45f580f3
Knackie <43431712+Knackie@users.noreply.github.com>
2022-11-04 19:30:52 +0100

    Update classement-elo.js

--- core/classement-elo.js
+++ core/classement-elo.js
@@ -43,72 +43,19 @@ fetch(config.sources.players)
 			whiteElo = whiteElo + 20 * (0.5 - oddsWhite);
 			console.log(whiteElo);
 			console.log(blackElo);  
-			beginClassement = "Égalité de <a href='players/";
-			beginClassement += data.matches[i].white.name;
-			beginClassement += "'>";
-			beginClassement += data.matches[i].white.name;
-			beginClassement += "</a>";
-			beginClassement += " avec les blancs contre <a href='players/";
-			beginClassement += data.matches[i].black.name;
-			beginClassement += "'>";
-			beginClassement += data.matches[i].black.name;
-			beginClassement += "</a>";
 		  } else if ([data.matches[i].winner] != "Draw") {
 			if (data.matches[i].winner == "white") {
 			  whiteElo = whiteElo + 20 * (1 - oddsBlack);
 			  blackElo = blackElo + 20 * (0 - oddsWhite);
 			console.log(whiteElo);
 			console.log(blackElo); 
-			  beginClassement = "Victoire de <a href='players/";
-			  beginClassement += data.matches[i].white.name;
-			  beginClassement += "'>";
-			  beginClassement += data.matches[i].white.name;
-			  beginClassement += "</a> avec les blancs contre <a href='players/";
-			  beginClassement += data.matches[i].black.name;
-			  beginClassement += "'>";
-			  beginClassement += data.matches[i].black.name;
-			  beginClassement += "</a>";
 			} else {
 			  blackElo = blackElo + 20 * (1 - oddsBlack);
 			  whiteElo = whiteElo + 20 * (0 - oddsWhite);
 			console.log(whiteElo);
 			console.log(blackElo); 
-			  beginClassement = "Victoire de <a href='players/";
-			  beginClassement += data.matches[i].black.name;
-			  beginClassement += "'>";
-			  beginClassement += data.matches[i].black.name;
-			  beginClassement += "</a> avec les noirs contre <a href='players/";
-			  beginClassement += data.matches[i].white.name;
-			  beginClassement += "'>";
-			  beginClassement += data.matches[i].white.name;
-			  beginClassement += "</a>";
 			}
 		  }
-		  beginClassement += " le ";
-		  beginClassement += data.matches[i].date;
-		  beginClassement += " ouverture : ";
-		  beginClassement += data.matches[i].opening;
-		  player.players[whiteIndex].elo = Math.floor(whiteElo);
-		  player.players[blackIndex].elo = Math.floor(blackElo);
-		  beginClassement += "Elo blanc : ";
-		  beginClassement += player.players[whiteIndex].elo;
-		  beginClassement += "Elo noir : ";
-		  beginClassement += player.players[blackIndex].elo;
-		  var divId = "div" + i;
-		  var divId = "div" + i;
-		  var tag = document.createElement(divId);
-
-		  var text = document.createTextNode("joueur1");
-
-		  var refJoueur1 = document.createTextNode("joueur1");
-		  var beginClassementRef = document.createTextNode(beginClassement);
-
-		  tag.appendChild(beginClassementRef);
-
-		  tag.classList.add("shadow");
-		  var element = document.getElementById("Classement-elo");
-		  element.appendChild(tag);
-		  document.querySelector(divId).innerHTML = beginClassement;
 		}
 	  });
   });
@@ -148,7 +95,7 @@ const createRankEl = (rank, playerName, elo) => {
   tag.style = "cursor: pointer";
   tag.appendChild(text);
 
-  const element = document.getElementById("Classement");
+  const element = document.getElementById("Classement-elo");
   element.appendChild(tag);
 	
 };
